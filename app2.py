8518056fd17b99c1cb7dad9ea622d4aaa73c9341
Hikmet Emre GÜLER <114312139+HikmetEmre@users.noreply.github.com>
2024-05-27 14:17:29 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -39,11 +39,12 @@ if uploaded_files:
             df = pd.read_csv(uploaded_file, low_memory=False)
         elif 'df2.csv' in uploaded_file.name:
             df = pd.read_csv(uploaded_file, skiprows=1, low_memory=False)
-         elif'df3.csv' in uploaded_file.name:
-              df = pd.read_csv(uploaded_file, skiprows=1, low_memory=False)
+        elif 'df3.csv' in uploaded_file.name:
+            df = pd.read_csv(uploaded_file, skiprows=1, low_memory=False)
         else:
             df = pd.read_csv(uploaded_file, low_memory=False)  # Default behavior for other files
         dfs.append(df)
+
     
     # Concatenate dataframes
     if len(dfs) > 0:
